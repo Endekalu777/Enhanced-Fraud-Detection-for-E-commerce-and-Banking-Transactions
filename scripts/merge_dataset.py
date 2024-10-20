3e8cc54ff13c2087f94bddbaafcb92a4997afe08
Endekalu Simon <endekalu.simon.haile@gmail.com>
2024-10-20 20:01:37 +0300

    Fix: Resolved 'NoneType' object has no attribute 'to_csv' error
    - Fixed an issue where the `match_ip_to_country` method was returning None, causing an error when calling `to_csv`.
    - Updated the `match_ip_to_country` method to return the modified fraud data with the 'country' column after IP matching.
    - Verified that the merged data is now properly saved to the specified file.

--- scripts/merge_dataset.py
+++ scripts/merge_dataset.py
@@ -21,8 +21,8 @@ class MergeDataset:
     def __init__(self, file_path1, file_path2):
         try:
             logging.info(f"Loading datasets from {file_path1} and {file_path2}")
-            self.df1 = pd.read_csv(file_path1)
-            self.df2 = pd.read_csv(file_path2)
+            self.fraud_data = pd.read_csv(file_path1)
+            self.ip_data = pd.read_csv(file_path2)
             logging.info("Successfully loaded the datasets")
         except FileNotFoundError as e:
             logging.error(f"File not found: {e}")
@@ -58,8 +58,8 @@ class MergeDataset:
     def save_merged_data(self, save_location):
         # Save the merged DataFrame to a CSV file and log the operation
         try:
-            merged_data = self.merge_df()
-            merged_data.to_csv(save_location, index=False)
+            self.match_ip_to_country()
+            self.fraud_data.to_csv(save_location, index=False)
             logging.info(f"Merged data saved to {save_location}.")
             print(f"Merged data saved to {save_location}")
         except Exception as e:
