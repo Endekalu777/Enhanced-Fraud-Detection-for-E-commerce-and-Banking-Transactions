721e9eafb17246a191cb94476c809f89efd6d6e0
Endekalu Simon <endekalu.simon.haile@gmail.com>
2024-10-20 12:25:10 +0300

    docs: Enhance logging configuration and code documentation
    - Added logging configuration to track data loading, merging, and saving processes.
    - Included comments throughout the code for improved clarity and understanding.
    - Updated the merge_df method to store the concatenation result in a variable before returning it.

--- scripts/merge_dataset.py
+++ scripts/merge_dataset.py
@@ -18,14 +18,23 @@ logging.basicConfig(filename=log_file,
 
 class merge_dataset():
     def init(self, file_path1, file_path2):
+        logging.info(f"Loading datasets from {file_path1} and {file_path2}")
         self.df1 = pd.read_csv(file_path1)
         self.df2 = pd.read_csv(file_path2)
+        logging.info(f"Successfully loaded the dataset")
 
     def merge_df(self):
-        return pd.concat([self.df1, self.df2], ignore_index = True)
+        # Merge the dataframes and log the operation
+        logging.info("Merging datasets.")
+        merged_data = pd.concat([self.df1, self.df2], ignore_index = True)
+        logging.info("Datasets merged successfully")
+        return merged_data
+    
     def save_merged_data(self, save_location):
+        # Save the merged DataFrame to a CSV file and log the operation
         merged_data = self.merge_df()
         merged_data.to_csv(save_location, index = False)
+        logging.info(f"Merged data saved to {save_location}.")
         print(f"Merged data saved to {save_location}")
 
     
